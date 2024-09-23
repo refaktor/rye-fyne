f32889309b324fdf9fb5ad17e62bb4909b3fcdfa
xypwn <54681180+xypwn@users.noreply.github.com>
2024-09-23 15:42:54 +0200

    update to latest ryegen

--- current/builtins_current.go
+++ current/builtins_current.go
@@ -2,7 +2,7 @@ package current
 
 import (
 	"github.com/refaktor/rye-front/current/ebitengine"
-	"github.com/refaktor/rye-front/current/fyne"
+	"github.com/refaktor/rye-front/current/fyne_io_fyne_v2"
 	"github.com/refaktor/rye-front/current/webview"
 	"github.com/refaktor/rye/env"
 	"github.com/refaktor/rye/evaldo"
@@ -13,6 +13,6 @@ var Builtins_current = map[string]*env.Builtin{}
 func RegisterBuiltins(ps *env.ProgramState) {
 	evaldo.RegisterBuiltins2(Builtins_current, ps, "current")
 	evaldo.RegisterBuiltinsInContext(ebitengine.Builtins_ebitengine, ps, "ebitengine")
-	evaldo.RegisterBuiltinsInContext(fyne.Builtins, ps, "fyne")
+	evaldo.RegisterBuiltinsInContext(fyne_io_fyne_v2.Builtins, ps, "fyne")
 	evaldo.RegisterBuiltinsInContext(webview.Builtins_webview, ps, "webview")
 }
